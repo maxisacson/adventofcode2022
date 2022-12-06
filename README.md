6d81bb43d34990a84bd3e65a345b2f96e138ad1f
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-06 19:33:37 +0100

    day 6 part 2

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://adventofcode.com/2022
 - [x] Day 3
 - [x] Day 4
 - [x] Day 5
-- [ ] Day 6
+- [x] Day 6
 - [ ] Day 7
 - [ ] Day 8
 - [ ] Day 9
--- day6/day6.go
+++ day6/day6.go
@@ -23,23 +23,49 @@ func main() {
 	var insert struct{}
 
 	fmt.Println("--- Part 1 ---")
-	var result int
+	var startOfPacket int
+
 	for _, line := range lines {
+		startOfPacket = 0
 		for i := 3; i < len(line); i++ {
-			set := make(map[byte]struct{})
+			markerSet := make(map[byte]struct{})
+
 			for j := 3; j >= 0; j-- {
-				set[line[i-j]] = insert
+				markerSet[line[i-j]] = insert
 			}
 
-			if len(set) == 4 {
-				result = i + 1
-				fmt.Println("Marker:", result, line[i-3:i+1])
-				break
+			if startOfPacket == 0 && len(markerSet) == 4 {
+				startOfPacket = i + 1
+				fmt.Println("Marker:", startOfPacket, line[i-3:i+1])
 			}
 		}
 	}
 
-	if result != 1929 && os.Args[1] == "input.txt" {
+	if startOfPacket != 1929 && os.Args[1] == "input.txt" {
 		panic("wrong answer!")
 	}
+
+	fmt.Println("--- Part 2 ---")
+	var startOfMessage int
+
+	for _, line := range lines {
+		startOfMessage = 0
+		for i := 13; i < len(line); i++ {
+			messageSet := make(map[byte]struct{})
+
+			for j := 13; j >= 0; j-- {
+				messageSet[line[i-j]] = insert
+			}
+
+			if startOfMessage == 0 && len(messageSet) == 14 {
+				startOfMessage = i + 1
+				fmt.Println("Message:", startOfMessage, line[i-13:i+1])
+			}
+		}
+	}
+
+	if startOfMessage != 3298 && os.Args[1] == "input.txt" {
+		panic("wrong answer!")
+	}
+
 }
