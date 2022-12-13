806957dca243e3d742a2495caee936fcfcad9935
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-13 21:02:53 +0100

    day 13 part 2

--- day13/day13.go
+++ day13/day13.go
@@ -4,6 +4,7 @@ import (
 	"aoc22/utils"
 	"bytes"
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -79,7 +80,7 @@ func Compare(a, b ListItem) int {
 		if a.Value < b.Value {
 			return 1
 		} else if b.Value < a.Value {
-			return 0
+			return -1
 		}
 	} else if a.Type == List && b.Type == List {
 		return CompareLists(a.List, b.List)
@@ -89,7 +90,7 @@ func Compare(a, b ListItem) int {
 		return CompareLists(a.List, []ListItem{b})
 	}
 
-	return -1
+	return 0
 }
 
 func CompareLists(a, b []ListItem) int {
@@ -101,7 +102,7 @@ func CompareLists(a, b []ListItem) int {
 
 	for i := 0; i < minLen; i++ {
 		result := Compare(a[i], b[i])
-		if result == -1 {
+		if result == 0 {
 			continue
 		}
 		return result
@@ -113,10 +114,10 @@ func CompareLists(a, b []ListItem) int {
 	}
 
 	if len(b) < len(a) {
-		return 0
+		return -1
 	}
 
-	return -1
+	return 0
 }
 
 func Run(fileName string) Result {
@@ -130,13 +131,14 @@ func Run(fileName string) Result {
 		}
 	}
 
+	// Part 1
 	sum := 0
 	for i := 0; i < len(pairs); i += 2 {
 		a := pairs[i]
 		b := pairs[i+1]
 		result := CompareLists(a, b)
-		if result < 0 {
-			panic("expected result > 0")
+		if result == 0 {
+			panic("expected result != 0")
 		}
 		if result == 1 {
 			index := i/2 + 1
@@ -144,5 +146,31 @@ func Run(fileName string) Result {
 		}
 	}
 
-	return Result{sum, 0}
+	// Part 2
+	div1 := ParseList("[[2]]")
+	div2 := ParseList("[[6]]")
+	pairs = append(pairs, div1)
+	pairs = append(pairs, div2)
+
+	sort.Slice(pairs, func(i, j int) bool {
+		result := CompareLists(pairs[i], pairs[j])
+		if result == 0 {
+			panic("expected result != 0")
+		}
+
+		return result == 1
+	})
+
+	index1 := 0
+	index2 := 0
+	for i, list := range pairs {
+		if CompareLists(list, div1) == 0 {
+			index1 = i + 1
+		}
+		if CompareLists(list, div2) == 0 {
+			index2 = i + 1
+		}
+	}
+
+	return Result{sum, index1 * index2}
 }
