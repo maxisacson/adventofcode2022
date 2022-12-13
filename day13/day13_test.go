6e604412364ce4222c5599da0926a0b6b26d929d
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-13 20:45:02 +0100

    day 13 part 1

---
+++ day13/day13_test.go
@@ -0,0 +1,26 @@
+package day13
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDay13(t *testing.T) {
+	fmt.Println("===== Day 13 =====")
+
+	{
+		expected := Result{13, 0}
+		actual := Run("example.txt")
+		if actual != expected {
+			t.Errorf("Expected: %d but got: %d\n", expected, actual)
+		}
+	}
+
+	{
+		expected := Result{4809, 0}
+		actual := Run("input.txt")
+		if actual != expected {
+			t.Errorf("Expected: %d but got: %d\n", expected, actual)
+		}
+	}
+}
