9f0cb3b95b6456399dc7352933e03f562e13ef99
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-20 18:44:53 +0100

    WIP: day 20

---
+++ day20/day20_test.go
@@ -0,0 +1,26 @@
+package day20
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDay20(t *testing.T) {
+	fmt.Println("===== Day 20 =====")
+
+	{
+		expected := Result{0, 0}
+		actual := Run("example.txt")
+		if actual != expected {
+			t.Errorf("Expected: %d but got: %d\n", expected, actual)
+		}
+	}
+
+	// {
+	// 	expected := Result{0, 0}
+	// 	actual := Run("input.txt")
+	// 	if actual != expected {
+	// 		t.Errorf("Expected: %d but got: %d\n", expected, actual)
+	// 	}
+	// }
+}
