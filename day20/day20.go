b60a8c94afab8acb4755fd74a0b15ea0abed01ca
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-23 19:10:13 +0100

    WIP: part 20

--- day20/day20.go
+++ day20/day20.go
@@ -11,14 +11,6 @@ type Result struct {
 	part2 int
 }
 
-func AppendList(a, b []int) []int {
-	for _, x := range b {
-		a = append(a, x)
-	}
-
-	return a
-}
-
 func Insert(list []int, index, value int) []int {
 	list = append(list, 0)
 	copy(list[index+1:], list[index:])
@@ -27,38 +19,53 @@ func Insert(list []int, index, value int) []int {
 	return list
 }
 
-func MixOnce(numbers []int, order []int) ([]int, []int) {
+func Pop(list *[]int) int {
+	n := len(*list) - 1
+	x := (*list)[n]
+	*list = (*list)[:n]
 
-	index := order[len(order)-1]
-	order = order[:len(order)-1]
+	return x
+}
 
-	newIndex := (index + numbers[index]) % len(numbers)
-	for newIndex < 0 {
-		newIndex += len(numbers)
-	}
-	fmt.Printf("%d -> %d\n", index, newIndex)
-
-	left := make([]int, index)
-	right := make([]int, len(numbers)-index-1)
-	copy(left, numbers[:index])
-	copy(right, numbers[index+1:])
+func Move(list *[]int, oldIndex, newIndex int) {
+	N := len(*list)
+	left := make([]int, oldIndex)
+	right := make([]int, N-oldIndex-1)
+	copy(left, (*list)[:oldIndex])
+	copy(right, (*list)[oldIndex+1:])
 
 	if newIndex < len(left) {
-		left = Insert(left, newIndex, numbers[index])
+		left = Insert(left, newIndex, (*list)[oldIndex])
 	} else {
 		rightIndex := newIndex - len(left)
-		right = Insert(right, rightIndex, numbers[index])
+		right = Insert(right, rightIndex, (*list)[oldIndex])
 	}
 
-	left = append(left, right...)
+	*list = append(left, right...)
+}
 
-	for i := len(order) - 1; i >= 0; i-- {
-		if order[i] <= newIndex-index {
-			order[i]--
+func MixOnce(numbers, stack []int) ([]int, []int) {
+
+	index := Pop(&stack)
+	N := len(numbers)
+
+	newIndex := (index + numbers[index]) % (N - 1)
+	for newIndex < 0 {
+		newIndex += (N - 1)
+	}
+	if newIndex == 0 && numbers[index] < 0 {
+		newIndex = N - 1
+	}
+
+	Move(&numbers, index, newIndex)
+
+	for i := 0; i < len(stack); i++ {
+		if index < stack[i] && stack[i] <= newIndex {
+			stack[i]--
 		}
 	}
 
-	return left, order
+	return numbers, stack
 }
 
 func Run(fileName string) Result {
@@ -71,29 +78,95 @@ func Run(fileName string) Result {
 		order[i] = len(lines) - 1 - i
 	}
 
-	fmt.Println(numbers)
-	fmt.Println(order)
+	// part 1
+	numbers1 := make([]int, len(numbers))
+	order1 := make([]int, len(order))
+	copy(numbers1, numbers)
+	copy(order1, order)
+	for len(order1) > 0 {
+		numbers1, order1 = MixOnce(numbers1, order1)
+	}
+	for _, n := range numbers {
+		fmt.Print(n, ", ")
+	}
+	fmt.Println()
+	for _, val := range order1 {
+		fmt.Print(numbers1[val], ", ")
+	}
 	fmt.Println()
 
-	numbers, order = MixOnce(numbers, order)
-	fmt.Println(numbers)
-	fmt.Println(order)
-	fmt.Println()
+	index := 0
+	for i, val := range numbers1 {
+		if val == 0 {
+			index = i
+			break
+		}
+	}
 
-	numbers, order = MixOnce(numbers, order)
-	fmt.Println(numbers)
-	fmt.Println(order)
-	fmt.Println()
+	sum := numbers1[(index+1000)%len(numbers1)]
+	sum += numbers1[(index+2000)%len(numbers1)]
+	sum += numbers1[(index+3000)%len(numbers1)]
 
-	numbers, order = MixOnce(numbers, order)
-	fmt.Println(numbers)
-	fmt.Println(order)
-	fmt.Println()
+	// part 2
+	key := 811589153
+	// key := 1
+	numbers2 := make([]int, len(numbers))
+	orderMap := map[int]int{}
+	for i, val := range numbers {
+		newVal := key * val
+		numbers2[i] = newVal
+		orderMap[newVal] = i
+	}
+	fmt.Println(orderMap)
 
-	numbers, order = MixOnce(numbers, order)
-	fmt.Println(numbers)
-	fmt.Println(order)
+	// fmt.Println(numbers2)
 	fmt.Println()
+	for round := 0; round < 10; round++ {
+		order2 := make([]int, len(numbers2))
+		for i, val := range numbers2 {
+			j := orderMap[val]
+			order2[len(order2)-1-j] = i
+		}
+		fmt.Println(order2)
+		for len(order2) > 0 {
+			numbers2, order2 = MixOnce(numbers2, order2)
+		}
+		// fmt.Println(numbers2)
+		// fmt.Println(order2)
+		// for _, n := range numbers {
+		// 	fmt.Print(n, ", ")
+		// }
+		// fmt.Println()
+		// for _, val := range order2 {
+		// 	// j := len(order2) - i - 1
+		// 	fmt.Print(numbers2[val], ", ")
+		//
+		// 	// if numbers[j] != numbers2[val] {
+		// 	// 	panic("help!")
+		// 	// }
+		// 	// if numbers[len(order2)-i-1] != numbers2[val] {
+		// 	// 	panic("help!")
+		// 	// }
+		// }
+		// fmt.Println()
+		fmt.Println(numbers2)
+		fmt.Println()
+	}
 
-	return Result{len(lines), len(lines)}
+	index = 0
+	for i, val := range numbers2 {
+		if val == 0 {
+			index = i
+			break
+		}
+	}
+	// fmt.Println(numbers2[(index+1000)%len(numbers2)])
+	// fmt.Println(numbers2[(index+2000)%len(numbers2)])
+	// fmt.Println(numbers2[(index+3000)%len(numbers2)])
+
+	sum2 := numbers2[(index+1000)%len(numbers2)]
+	sum2 += numbers2[(index+2000)%len(numbers2)]
+	sum2 += numbers2[(index+3000)%len(numbers2)]
+
+	return Result{sum, sum2}
 }
