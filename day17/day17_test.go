86c9ee12efdd6256b838badec24c88aae787a6f0
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-26 14:21:52 +0100

    day 17 part 2

--- day17/day17_test.go
+++ day17/day17_test.go
@@ -9,7 +9,7 @@ func TestDay17(t *testing.T) {
 	fmt.Println("===== Day 17 =====")
 
 	{
-		expected := Result{3068, 0}
+		expected := Result{3068, 1514285714288}
 		actual := Run("example.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
@@ -17,7 +17,7 @@ func TestDay17(t *testing.T) {
 	}
 
 	{
-		expected := Result{3186, 0}
+		expected := Result{3186, 1566376811584}
 		actual := Run("input.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
