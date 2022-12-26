40073201c2735a9e181cbde787c476ddfda79655
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-26 17:53:00 +0100

    day 21 part 2

--- day21/day21.go
+++ day21/day21.go
@@ -19,12 +19,25 @@ type Node struct {
 	op       string
 	lhs      *Node
 	rhs      *Node
+
+	isVariable bool
 }
 
-func (n *Node) Eval() int {
-	if !n.hasValue {
-		lhs := n.lhs.Eval()
-		rhs := n.rhs.Eval()
+func (n *Node) Eval() {
+	if n.hasValue {
+		return
+	}
+
+	if n.isVariable {
+		return
+	}
+
+	n.lhs.Eval()
+	n.rhs.Eval()
+
+	if n.lhs.hasValue && n.rhs.hasValue {
+		lhs := n.lhs.value
+		rhs := n.rhs.value
 
 		switch n.op {
 		case "+":
@@ -43,21 +56,102 @@ func (n *Node) Eval() int {
 		default:
 			panic(fmt.Sprintf("Unknown op: %s", n.op))
 		}
+		n.hasValue = true
+	}
+}
+
+func (n *Node) Solve() {
+	for !n.lhs.isVariable && !n.rhs.isVariable {
+		n.Eval()
+
+		if n.lhs.hasValue && n.rhs.hasValue {
+			return
+		}
+
+		if !n.lhs.hasValue && !n.rhs.hasValue {
+			PrintTree(n, 0)
+			panic("At least one subtree needs to be known!")
+		}
+
+		if !n.lhs.hasValue {
+			invOp := ""
+			switch n.lhs.op {
+			case "+":
+				invOp = "-"
+			case "-":
+				invOp = "+"
+			case "/":
+				invOp = "*"
+			case "*":
+				invOp = "/"
+			}
+
+			node := new(Node)
+			node.name = "tmp"
+			node.op = invOp
+
+			if !n.lhs.rhs.hasValue {
+				if n.lhs.op == "+" || n.lhs.op == "*" {
+					tmp := n.lhs.rhs
+					n.lhs.rhs = n.lhs.lhs
+					n.lhs.lhs = tmp
+				}
+			}
+
+			node.rhs = n.lhs.rhs
+			node.lhs = n.rhs
+			n.rhs = node
+			n.lhs = n.lhs.lhs
+		} else {
+			tmp := n.lhs
+			n.lhs = n.rhs
+			n.rhs = tmp
+		}
 	}
 
-	n.hasValue = true
-	return n.value
+	n.Eval()
+	if !n.lhs.isVariable {
+		tmp := n.lhs
+		n.lhs = n.rhs
+		n.rhs = tmp
+	}
 }
 
 func BuildTree(name string, nodes *map[string]Node) *Node {
 	node := (*nodes)[name]
 
-	if !node.hasValue {
+	if !node.hasValue && !node.isVariable {
 		node.lhs = BuildTree(node.lhs.name, nodes)
 		node.rhs = BuildTree(node.rhs.name, nodes)
 	}
 
 	return &node
+}
+
+func PrintTree(root *Node, indentLevel int) {
+	indent := strings.Repeat(" ", indentLevel)
+	if root.hasValue {
+		fmt.Printf("%s%s: %d\n", indent, root.name, root.value)
+	} else if root.isVariable {
+		fmt.Printf("%s%s: X\n", indent, root.name)
+	} else {
+		fmt.Printf("%s%s: %s [\n", indent, root.name, root.op)
+		PrintTree(root.lhs, indentLevel+2)
+		PrintTree(root.rhs, indentLevel+2)
+		fmt.Printf("%s]\n", indent)
+	}
+}
+
+func (n Node) String() string {
+	if n.isVariable {
+		return "X"
+	} else if n.hasValue {
+		return fmt.Sprint(n.value)
+	} else if n.op == "=" {
+		return fmt.Sprintf("%v %s %v", n.lhs, n.op, n.rhs)
+	}
+
+	return fmt.Sprintf("(%v%s%v)", n.lhs, n.op, n.rhs)
 }
 
 func Run(fileName string) Result {
@@ -86,8 +180,24 @@ func Run(fileName string) Result {
 		nodes[node.name] = node
 	}
 
-	root := BuildTree("root", &nodes)
-	result := root.Eval()
+	nodesCopy := nodes
 
-	return Result{result, 0}
+	// Part 1
+	root := BuildTree("root", &nodes)
+	root.Eval()
+	result := root.value
+
+	// Part 2
+	root2 := nodesCopy["root"]
+	root2.op = "="
+	nodesCopy["root"] = root2
+	humn := nodesCopy["humn"]
+	humn.hasValue = false
+	humn.isVariable = true
+	nodesCopy["humn"] = humn
+
+	root = BuildTree("root", &nodesCopy)
+	root.Solve()
+
+	return Result{result, root.rhs.value}
 }
--- day21/day21_test.go
+++ day21/day21_test.go
@@ -9,7 +9,7 @@ func TestDay21(t *testing.T) {
 	fmt.Println("===== Day 21 =====")
 
 	{
-		expected := Result{152, 0}
+		expected := Result{152, 301}
 		actual := Run("example.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
@@ -17,7 +17,7 @@ func TestDay21(t *testing.T) {
 	}
 
 	{
-		expected := Result{93813115694560, 0}
+		expected := Result{93813115694560, 3910938071092}
 		actual := Run("input.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
