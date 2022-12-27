bebc442a60337a86aebafd31f225889f0e060513
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-27 23:19:01 +0100

    WIP day 22 part 2

--- day22/day22_test.go
+++ day22/day22_test.go
@@ -9,7 +9,7 @@ func TestDay22(t *testing.T) {
 	fmt.Println("===== Day 22 =====")
 
 	{
-		expected := Result{6032, 0}
+		expected := Result{6032, 5031}
 		actual := Run("example.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
