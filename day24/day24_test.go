ddda8b4508b616b42040a70d00b8067d2af6c729
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-29 11:43:49 +0100

    WIP: day 24 part 2

--- day24/day24_test.go
+++ day24/day24_test.go
@@ -9,7 +9,7 @@ func TestDay24(t *testing.T) {
 	fmt.Println("===== Day 24 =====")
 
 	{
-		expected := Result{18, 0}
+		expected := Result{18, 54}
 		actual := Run("example.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
@@ -17,7 +17,7 @@ func TestDay24(t *testing.T) {
 	}
 
 	{
-		expected := Result{0, 0}
+		expected := Result{251, 0}
 		actual := Run("input.txt")
 		if actual != expected {
 			t.Errorf("Expected: %d but got: %d\n", expected, actual)
