debb09695473f5df0e8b4dd20c30cf98dce100ba
Max Isacson <5937020+maxisacson@users.noreply.github.com>
2022-12-29 12:29:08 +0100

    day 24 part 2

--- day24/day24.go
+++ day24/day24.go
@@ -2,7 +2,6 @@ package day24
 
 import (
 	"aoc22/utils"
-	"fmt"
 	"strings"
 )
 
@@ -47,25 +46,17 @@ func Pop(list *[]byte) byte {
 	return r
 }
 
-type Node struct {
-	pos  Vec
-	next []*Node
-	prev *Node
-	dist int
-}
-
 type Board struct {
 	tiles    [][]Tile
 	start    Vec
 	goal     Vec
-	frontier []*Node
-	ends     []*Node
+	frontier []Vec
+	done     bool
 }
 
 func (b Board) String() string {
 	nRows := len(b.tiles)
-	offset := len(b.tiles[0]) + 1
-	nCols := 2*len(b.tiles[0]) + 1
+	nCols := len(b.tiles[0])
 
 	bytes := make([][]byte, nRows)
 	for i, row := range b.tiles {
@@ -74,23 +65,13 @@ func (b Board) String() string {
 			if tile.tile == 'B' {
 				if len(tile.blizzards) == 1 {
 					bytes[i][j] = tile.blizzards[0]
-					bytes[i][j+offset] = tile.blizzards[0]
 				} else {
 					bytes[i][j] = byte('0' + len(tile.blizzards))
-					bytes[i][j+offset] = byte('0' + len(tile.blizzards))
 				}
 			} else {
 				bytes[i][j] = tile.tile
-				bytes[i][j+offset] = tile.tile
 			}
-			// bytes[i][j] = tile.tile
-			// bytes[i][j+offset] = tile.tile
-			bytes[i][offset-1] = ' '
 		}
-	}
-
-	for _, n := range b.frontier {
-		bytes[n.pos.y][n.pos.x+offset] = '@'
 	}
 
 	lines := make([]string, nRows)
@@ -203,73 +184,31 @@ func (b *Board) Round() {
 	}
 	b.tiles = tiles
 
-	frontierMap := map[Vec]*Node{}
+	frontierSet := map[Vec]bool{}
 	for _, n := range b.frontier {
-		for _, p := range b.Adjacent(n.pos.x, n.pos.y) {
-			if n.prev != nil && p == n.prev.pos {
-				continue
-			}
+		for _, p := range b.Adjacent(n.x, n.y) {
 			if b.IsBlizzard(p.x, p.y) {
 				continue
 			}
-			next := new(Node)
-			next.prev = n
-			next.pos = p
-			next.dist = n.dist + 1
-			tmp, ok := frontierMap[p]
-			if ok {
-				if next.dist < tmp.dist {
-					*tmp = *next
-				}
-			} else {
-				frontierMap[p] = next
-			}
-			n.next = append(n.next, frontierMap[p])
+
+			frontierSet[p] = true
 
 			if p == b.goal {
-				b.ends = append(b.ends, next)
+				b.done = true
 			}
-			// fmt.Println(p)
 		}
-		if !b.IsBlizzard(n.pos.x, n.pos.y) {
-			next := new(Node)
-			next.prev = n
-			next.pos = n.pos
-			next.dist = n.dist + 1
-			n.next = append(n.next, next)
-			frontierMap[next.pos] = next
+		if !b.IsBlizzard(n.x, n.y) {
+			frontierSet[n] = true
 		}
 	}
 
-	frontier := []*Node{}
+	frontier := []Vec{}
 
-	for _, v := range frontierMap {
-		frontier = append(frontier, v)
+	for p := range frontierSet {
+		frontier = append(frontier, p)
 	}
 
 	b.frontier = frontier
-}
-
-func (b *Board) ShortestPath() []Vec {
-	path := []Vec{}
-
-	var next *Node
-	next = nil
-	minDist := -1
-
-	for _, n := range b.ends {
-		if minDist == -1 || n.dist < minDist {
-			minDist = n.dist
-			next = n
-		}
-	}
-
-	for next != nil {
-		path = append(path, next.pos)
-		next = next.prev
-	}
-
-	return path
 }
 
 func MakeBoard(tiles [][]Tile, start, goal Vec) Board {
@@ -277,9 +216,9 @@ func MakeBoard(tiles [][]Tile, start, goal Vec) Board {
 	board.start = start
 	board.goal = goal
 	board.tiles = make([][]Tile, len(tiles))
+	board.done = false
 	copy(board.tiles, tiles)
-	root := Node{start, []*Node{}, nil, 0}
-	board.frontier = []*Node{&root}
+	board.frontier = []Vec{start}
 	return board
 }
 
@@ -308,26 +247,26 @@ func Run(fileName string) Result {
 	board := MakeBoard(tiles, start, goal)
 
 	// Part 1
-	for len(board.ends) == 0 {
+	steps := 0
+	for !board.done {
 		board.Round()
+		steps++
 	}
-	steps := len(board.ShortestPath()) - 1
-	fmt.Println(steps, len(board.ends))
 
 	// Part 2
 	board = MakeBoard(board.tiles, goal, start)
-	for len(board.ends) == 0 {
+	steps2 := 0
+	for !board.done {
 		board.Round()
+		steps2++
 	}
-	steps2 := len(board.ShortestPath()) - 1
-	fmt.Println(steps2, len(board.ends))
 
 	board = MakeBoard(board.tiles, start, goal)
-	for len(board.ends) == 0 {
+	steps3 := 0
+	for !board.done {
 		board.Round()
+		steps3++
 	}
-	steps3 := len(board.ShortestPath()) - 1
-	fmt.Println(steps3, len(board.ends))
 
 	return Result{steps, steps + steps2 + steps3}
 }
